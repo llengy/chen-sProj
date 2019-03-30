3ae5e0d1b0b2c08f0b151391bf35960225d0cb3b
e <1451929954@qq.com>
2019-03-31 00:27:49 +0800

    修改3

--- src/views/categories/index.vue
+++ src/views/categories/index.vue
@@ -224,14 +224,16 @@ export default {
     },
     //设置商品图选中的数量
     setGoodsTotal(){
-      if(this.myCart && this.categoryList && this.goodsObj){
+      if(this.myCart){
         for(let i = 0;i<this.categoryList.length;i++){
-          for(let j = 0;j<this.goodsObj[this.categoryList[i].cat_no].length;j++){
-            for(let k=0;k<this.myCart.length;k++){
-              if(this.myCart[k].goods_no === this.goodsObj[this.categoryList[i].cat_no][j].goods_no){
-                this.$set(this.goodsObj[this.categoryList[i].cat_no][j], "total",this.myCart[k].total)
+          if(typeof this.goodsObj[this.categoryList[i].cat_no] !== 'undefined'){
+            Object.keys(this.goodsObj[this.categoryList[i].cat_no]).map((key,item)=>{
+              for(let k = 0;k <this.myCart.length;k++){
+              if(this.myCart[k].goods_no === this.goodsObj[this.categoryList[i].cat_no][key].goods_no){
+                this.$set(this.goodsObj[this.categoryList[i].cat_no][key], "total",this.myCart[k].total)
               }
             }
+          })
           }
         }
       }
@@ -239,12 +241,16 @@ export default {
     },
     //清空商品图选中的数量
     clearGoodsTotal(){
-      for(let i = 0;i<this.categoryList.length;i++){
-        for(let j = 0;j<this.goodsObj[this.categoryList[i].cat_no].length;j++){
-          for(let k=0;k<this.myCart.length;k++){
-            if(this.myCart[k].goods_no === this.goodsObj[this.categoryList[i].cat_no][j].goods_no){
-              this.$set(this.goodsObj[this.categoryList[i].cat_no][j], "total",  0)
+      if(this.myCart){
+        for(let i = 0;i<this.categoryList.length;i++){
+          if(typeof this.goodsObj[this.categoryList[i].cat_no] !== 'undefined'){
+            Object.keys(this.goodsObj[this.categoryList[i].cat_no]).map((key,item)=>{
+              for(let k = 0;k <this.myCart.length;k++){
+              if(this.myCart[k].goods_no === this.goodsObj[this.categoryList[i].cat_no][key].goods_no){
+                this.$set(this.goodsObj[this.categoryList[i].cat_no][key], "total",0)
+              }
             }
+          })
           }
         }
       }
